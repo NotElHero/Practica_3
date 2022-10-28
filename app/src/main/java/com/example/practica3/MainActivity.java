eddc0cc8ea63ba4329db3a43277555efda299da4
Javier Villacorta <javier.vilgar@educa.jcyl.es>
2022-10-28 11:29:05 +0200

    vYa funciona la movida de los colores y los switches so gucci v2

--- app/src/main/java/com/example/practica3/MainActivity.java
+++ app/src/main/java/com/example/practica3/MainActivity.java
@@ -78,6 +78,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 escogerColorTexto();
                 break;
         }
+
     }
 
     public void escogerColorFondo() {
