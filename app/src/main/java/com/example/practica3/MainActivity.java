8251b9d7d36b268fe733739202597b8981c1e776
Javier Villacorta <javier.vilgar@educa.jcyl.es>
2022-10-28 10:34:19 +0200

    vYa funciona la movida de los colores y los switches so gucci

--- app/src/main/java/com/example/practica3/MainActivity.java
+++ app/src/main/java/com/example/practica3/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.practica3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView contador;
+    EditText meta;
     Button botonMas, botonMenos, botonColorTexto, botonColorFondo;
     Switch switchx2, switchMenos0;
     int cont = 0;
+    int defaultColorFondo, defaultColorTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,22 +40,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switchx2 = findViewById(R.id.switchX2);
         switchMenos0 = findViewById(R.id.switchMenos0);
 
+        defaultColorFondo = ContextCompat.getColor(MainActivity.this, com.google.android.material.R.color.design_default_color_primary);
+        defaultColorTexto = ContextCompat.getColor(MainActivity.this, com.google.android.material.R.color.design_default_color_primary);
+
+        meta = findViewById(R.id.editTextNumber);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.botonMas:
-                cont++;
+                if (switchx2.isChecked()){
+                    cont+=2;
+                }else if (switchMenos0.isChecked() && cont < 0){
+                    cont = 0;
+                    cont++;
+                }else{
+                    cont++;
+                }
                 contador.setText(String.valueOf(cont));
                 break;
             case R.id.botonMenos:
-                cont--;
+                if (switchx2.isChecked()){
+                    cont-=2;
+                }else{
+                    cont--;
+                }
+                if (switchMenos0.isChecked() && cont < 0){
+                    cont = 0;
+                }
                 contador.setText(String.valueOf(cont));
                 break;
+            case R.id.botonColorFondo:
+                escogerColorFondo();
+                break;
             case R.id.botonColorTexto:
-
+                escogerColorTexto();
                 break;
         }
+    }
+
+    public void escogerColorFondo() {
+        AmbilWarnaDialog selector = new AmbilWarnaDialog(this, defaultColorFondo, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                defaultColorFondo = color;
+                contador.setBackgroundColor(defaultColorFondo);
+            }
+        });
+        selector.show();
+    }
+
+    public void escogerColorTexto(){
+        AmbilWarnaDialog selector2 = new AmbilWarnaDialog(this, defaultColorTexto, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                defaultColorTexto = color;
+                contador.setTextColor(defaultColorTexto);
+            }
+        });
+        selector2.show();
     }
 }
